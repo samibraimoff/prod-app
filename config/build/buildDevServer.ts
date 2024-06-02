1f826affda9e83f59937ea287e528b49ed98caf7
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2024-06-02 12:58:15 -0400

    fix gitignore

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import { BuildOptions } from "./types";
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+export const buildDerServer = (options: BuildOptions): DevServerConfiguration => {
+    return {
+        port: options.port,
+        open: true,
+        historyApiFallback: true
+    }
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import webpack from 'webpack';
+import { BuildOptions } from "./types"
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDerServer } from './buildDevServer';
+
+export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
+    const { mode, paths, isDev } = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(paths),
+        module: {
+            rules: buildLoaders(options)
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDerServer(options) : undefined
+    }
+}
