6849215e8670bdc4b5ada63f57a3a84da76f8fc6
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-01-06 23:31:06 -0600

    implement classNames helper function

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import "./themes/index.scss";
 import AboutPage from "./pages/about/about-page-lazy";
 import MainPage from "./pages/main/main-page-lazy";
 import { useTheme } from "./context/use-theme";
+import { classNames } from "./helpers/classnames/classnames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <Suspense fallback={"loading..."}>
-      <div className={`app ${theme}`}>
+      <div className={classNames("app", {}, [theme])}>
         <Link to={"/about"}>About</Link>
         <Link to={"/"}>Main</Link>
         <button onClick={toggleTheme}>theme</button>
