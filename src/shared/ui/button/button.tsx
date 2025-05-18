0e58a7256f5fe0d39d4b549951a015733cf8409e
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-05-18 02:36:30 -0500

    button update

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -5,23 +5,37 @@ import classes from './button.module.scss';
 export enum ButtonTheme {
   CLEAR = 'clear',
   OUTLINED = 'outlined',
-  PRIMARY = 'primary'
+  PRIMARY = 'primary',
+  BACKGROUND = 'background'
+}
+
+export enum ButtonSize {
+  M = 'size_m',
+  L = 'size_l',
+  XL = 'size_xl',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ButtonTheme;
+  square?: boolean;
+  size?: ButtonSize;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
   const {
-    className, children, theme, ...rest
+    className, children, theme, square, size, ...rest
   } = props;
+
+  const mods: Record<string, boolean> = {
+    [classes.square]: square,
+    [classes[size]]: true,
+  };
 
   return (
     <button
       type="button"
-      className={classNames(classes.button, {}, [className, classes[theme]])}
+      className={classNames(classes.button, mods, [className, classes[theme]])}
       {...rest}
     >
       {children}
