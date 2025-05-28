2ebead08bc19df71f801e4147d40c964b71f6990
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-05-28 00:15:22 -0500

    install json server

---
+++ json-server/index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+server.use(jsonServer.defaults());
+server.use(jsonServer.bodyParser);
+
+server.use(async (req, res, next) => {
+  await new Promise((res) => {
+    setTimeout(res, 500);
+  });
+  next();
+});
+
+server.post('/login', (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+    const { users = [] } = db;
+    const userFromDb = users.find((user) => user.username === username && user.password === password);
+    if (username) {
+      return res.json(userFromDb);
+    }
+
+    return res.status(403).json({ message: 'User not found' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// eslint-disable-next-lines consistent-return
+server.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    console.log(req.headers);
+    return res.status(403).json({ message: 'No token provided' });
+  }
+  next();
+});
+
+server.use(router);
+
+server.listen(8080, () => {
+  // eslint-disable-next-line no-console
+  console.log('JSON Server is running on Port 8080');
+});
