607560f38910ac2e5d8a1cb158480973ea1c910a
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-06-06 02:56:13 -0500

    remove user component in user entity

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,4 +1,3 @@
 export { getUserAuthData } from './model/selectors/get-user-auth-data/get-user-auth-data';
 export { userActions, userReducer } from './model/slice/user-slice';
 export { IUser, UserSchema } from './model/types/user-types';
-export { User } from './ui/user';
--- src/entities/user/ui/user.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import { User } from './user';
-
-describe('User entity', () => {
-  it('should render user entity', () => {
-    expect(true).toBe(true);
-  });
-});
