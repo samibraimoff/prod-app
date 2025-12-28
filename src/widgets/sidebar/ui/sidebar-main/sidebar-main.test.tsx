9087127c22df289deb5372a5602ca2ab7a6ef2a9
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-12-28 05:13:01 -0600

    update sidebar test and add snapshot

--- src/widgets/sidebar/ui/sidebar-main/sidebar-main.test.tsx
+++ src/widgets/sidebar/ui/sidebar-main/sidebar-main.test.tsx
@@ -1,19 +1,22 @@
 import { Sidebar } from "widgets/sidebar";
-import { fireEvent, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { componentRender } from "shared/helpers/component-render/component-render";
 
 describe("sidebar main", () => {
   test("should render sidebar main component", () => {
-    componentRender(<Sidebar />);
-    expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+    const { getByTestId } = componentRender(<Sidebar />);
+    expect(getByTestId("sidebar")).toBeVisible();
+    expect(getByTestId("sidebar")).toMatchSnapshot();
   });
 
-  test("should toggle sidebar", () => {
-    componentRender(<Sidebar />);
-    const toggleBtn = screen.getByTestId("sidebar-toggle");
-    expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+  test("should toggle sidebar", async () => {
+    const { getByTestId } = componentRender(<Sidebar />);
+    const user = userEvent.setup();
+    const toggleBtn = getByTestId("sidebar-toggle");
+    const sidebar = getByTestId("sidebar");
+    expect(sidebar).toBeVisible();
 
-    fireEvent.click(toggleBtn);
-    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
+    await user.click(toggleBtn);
+    expect(sidebar).toHaveClass("collapsed");
   });
 });
