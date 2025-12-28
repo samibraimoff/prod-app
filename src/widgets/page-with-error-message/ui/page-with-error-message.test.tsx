72b0e060e82a3389cf84009feef0ce114a8eed39
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-12-28 05:17:18 -0600

    add test error with message component and add snapshot

---
+++ src/widgets/page-with-error-message/ui/page-with-error-message.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import { PageWithErrorMessage } from "./page-with-error-message";
+
+describe("PageWithErrorMessage component", () => {
+  test("should render correctly", () => {
+    const { getByText } = render(<PageWithErrorMessage />);
+    const errorMessageElement = getByText("errorMessage.text");
+    expect(errorMessageElement).toBeVisible();
+    expect(errorMessageElement).toMatchSnapshot();
+  });
+});
