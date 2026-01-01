6c014d68b1273ee107c74b26283583dacfb06254
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2026-01-01 04:07:07 -0600

    remove test counter component from home page

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -1,14 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { cssClassNames } from "shared/helpers/class-names/css-class-names";
 import styles from "./Home.module.scss";
-import { Counter } from "entities/counter";
 
 const Home = () => {
   const { t } = useTranslation("main");
   return (
     <div className={cssClassNames(styles.Home, {}, [])}>
       <h2>{t("home")}</h2>
-      <Counter />
     </div>
   );
 };
